e8e6398d05639517caa1bd88f6f9c7f042d6e6a7
Shreyas Srinivasan <shreyas.srinivasan@searce.com>
2022-09-26 09:41:05 +0530

    done with the login screen

--- connectify/lib/login/login.dart
+++ connectify/lib/login/login.dart
@@ -1,11 +1,118 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Stack(children: [
+            Positioned(
+              top: -300,
+              right: -65,
+              child: SvgPicture.asset(
+                'assets/home/home-design.svg',
+                height: 800,
+                fit: BoxFit.scaleDown,
+              ),
+            ),
+            Center(
+              child: Column(
+                children: [
+                  const SizedBox(height: 250),
+                  SvgPicture.asset('assets/connectify-logo.svg', height: 120),
+                  Text('CONNECTIFY',
+                      style: GoogleFonts.montserrat(
+                          textStyle: const TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.bold))),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Text(
+                    'Sign in to continue.',
+                    style: TextStyle(fontSize: 15),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 50.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.grey[300],
+                          borderRadius: BorderRadius.circular(20.0)),
+                      child: const Padding(
+                        padding: EdgeInsets.only(left: 20),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            labelText: 'Email',
+                            border: InputBorder.none,
+                            hintText: 'jiaramartins@gmail.com',
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 50.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.grey[300],
+                          borderRadius: BorderRadius.circular(20.0)),
+                      child: const Padding(
+                        padding: EdgeInsets.only(left: 20),
+                        child: TextField(
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            labelText: 'Password',
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 40),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 50),
+                    child: Container(
+                      padding: const EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                          color: Theme.of(context).primaryColorDark,
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Center(
+                          child: Text(
+                        'Log in',
+                        style: TextStyle(
+                            color: Theme.of(context).primaryColorLight,
+                            fontWeight: FontWeight.bold),
+                      )),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text('New User? '),
+                      Text(
+                        'Sign up!',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ]),
+        ),
+      ),
+    );
   }
 }
--- connectify/lib/routes.dart
+++ connectify/lib/routes.dart
@@ -1,8 +1,7 @@
-import 'dart:js';
 import 'package:connectify/login/login.dart';
 import 'home/home.dart';
 
 var appRoutes = {
-  '/': (context) => const HomeScreen(),
-  '/login': (context) => const LoginScreen()
+  '/': (context) => const LoginScreen(),
+  // '/login': (context) => const LoginScreen()
 };
