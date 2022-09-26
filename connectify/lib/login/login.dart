f68701792a45c125277907f774d941c38893da2a
Shreyas Srinivasan <shreyas.srinivasan@searce.com>
2022-09-26 11:21:01 +0530

    removed Safe area and increased sized between form and button

--- connectify/lib/login/login.dart
+++ connectify/lib/login/login.dart
@@ -11,107 +11,105 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Stack(children: [
-            Positioned(
-              top: -300,
-              right: -65,
-              child: SvgPicture.asset(
-                'assets/home/home-design.svg',
-                height: 800,
-                fit: BoxFit.scaleDown,
-              ),
+      body: SingleChildScrollView(
+        child: Stack(children: [
+          Positioned(
+            top: -300,
+            right: -65,
+            child: SvgPicture.asset(
+              'assets/home/home-design.svg',
+              height: 800,
+              fit: BoxFit.scaleDown,
             ),
-            Center(
-              child: Column(
-                children: [
-                  const SizedBox(height: 250),
-                  SvgPicture.asset('assets/connectify-logo.svg', height: 120),
-                  Text('CONNECTIFY',
-                      style: GoogleFonts.montserrat(
-                          textStyle: const TextStyle(
-                              fontSize: 30, fontWeight: FontWeight.bold))),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  const Text(
-                    'Sign in to continue.',
-                    style: TextStyle(fontSize: 15),
-                  ),
-                  const SizedBox(
-                    height: 50,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 50.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                          color: Colors.grey[300],
-                          borderRadius: BorderRadius.circular(20.0)),
-                      child: const Padding(
-                        padding: EdgeInsets.only(left: 20),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            labelText: 'Email',
-                            border: InputBorder.none,
-                            hintText: 'jiaramartins@gmail.com',
-                          ),
+          ),
+          Center(
+            child: Column(
+              children: [
+                const SizedBox(height: 250),
+                SvgPicture.asset('assets/connectify-logo.svg', height: 120),
+                Text('CONNECTIFY',
+                    style: GoogleFonts.montserrat(
+                        textStyle: const TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold))),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  'Sign in to continue.',
+                  style: TextStyle(fontSize: 15),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 50.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.grey[300],
+                        borderRadius: BorderRadius.circular(20.0)),
+                    child: const Padding(
+                      padding: EdgeInsets.only(left: 20),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          border: InputBorder.none,
+                          hintText: 'jiaramartins@gmail.com',
                         ),
                       ),
                     ),
                   ),
-                  const SizedBox(height: 20),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 50.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                          color: Colors.grey[300],
-                          borderRadius: BorderRadius.circular(20.0)),
-                      child: const Padding(
-                        padding: EdgeInsets.only(left: 20),
-                        child: TextField(
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            labelText: 'Password',
-                            border: InputBorder.none,
-                          ),
+                ),
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 50.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.grey[300],
+                        borderRadius: BorderRadius.circular(20.0)),
+                    child: const Padding(
+                      padding: EdgeInsets.only(left: 20),
+                      child: TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          border: InputBorder.none,
                         ),
                       ),
                     ),
                   ),
-                  const SizedBox(height: 40),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 50),
-                    child: Container(
-                      padding: const EdgeInsets.all(15),
-                      decoration: BoxDecoration(
-                          color: Theme.of(context).primaryColorDark,
-                          borderRadius: BorderRadius.circular(10)),
-                      child: Center(
-                          child: Text(
-                        'Log in',
-                        style: TextStyle(
-                            color: Theme.of(context).primaryColorLight,
-                            fontWeight: FontWeight.bold),
-                      )),
-                    ),
+                ),
+                const SizedBox(height: 50),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 50),
+                  child: Container(
+                    padding: const EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColorDark,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Center(
+                        child: Text(
+                      'Log in',
+                      style: TextStyle(
+                          color: Theme.of(context).primaryColorLight,
+                          fontWeight: FontWeight.bold),
+                    )),
                   ),
-                  const SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Text('New User? '),
-                      Text(
-                        'Sign up!',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      )
-                    ],
-                  )
-                ],
-              ),
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text('New User? '),
+                    Text(
+                      'Sign up!',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    )
+                  ],
+                )
+              ],
             ),
-          ]),
-        ),
+          ),
+        ]),
       ),
     );
   }
