eee53df12a31aba57fc01d10d34e99f1417f68ce
Shreyas Srinivasan <shreyas.srinivasan@searce.com>
2022-09-26 11:44:54 +0530

    minor clean up on login page

--- connectify/lib/routes.dart
+++ connectify/lib/routes.dart
@@ -1,7 +1,9 @@
 import 'package:connectify/login/login.dart';
-import 'home/home.dart';
+// import 'home/home.dart';
+// import 'register/register.dart';
 
 var appRoutes = {
   '/': (context) => const LoginScreen(),
   // '/login': (context) => const LoginScreen()
+  // '/register': (context) => const RegisterScreen(),
 };
